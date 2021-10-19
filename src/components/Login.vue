c653e8a6c527b0a6d21bc9422a98eb9107c6c14c
CXG <2226787396@qq.com>
2021-10-19 17:23:01 +0800

    【代码】代码初修改+登录表单初制作-cxg

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avater_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+
+    .avater_box{
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        // background-color: #fff;//这种镂空还蛮好看的
+        height: 130px;
+        width: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+</style>
