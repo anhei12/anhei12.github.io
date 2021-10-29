325acbcd4af535a4eae1304bff4ae2f264755eea
CXG <2226787396@qq.com>
2021-10-29 15:02:18 +0800

    【代码复原+角色权限管理】-cxg

--- src/components/rights-management/roleList.vue
+++ src/components/rights-management/roleList.vue
@@ -16,7 +16,36 @@
 
             <el-table :data="roleList" border stripe>
                 <!-- 展开列 -->
-                <el-table-column type="expand"></el-table-column>
+                <el-table-column type="expand">
+                    <template slot-scope="scope">
+                        <el-row :class="['bdbottom',i1 == 0?'bdtop':'']" v-for = '(item1, i1) in scope.row.children' :key="item1.id">
+                            <!-- 总共有24列 -->
+                            <!-- 渲染一级权限 -->
+                            <el-col :span="5">
+                                <el-tag>{{item1.authName}}</el-tag>
+                                <i class="el-icon-caret-right"></i>
+                            </el-col>
+                            <!-- 渲染二级三级权限 -->
+                            <el-col :span="19">
+                                <!-- 通过for循环嵌套渲染二级权限 -->
+                                <el-row :class="['bdbottom',i2 == 0?'':'bdtop']" v-for="(item2,i2) in item1.children" :key="item2.id">
+                                    <el-col :span="6">
+                                        <el-tag type="success">{{item2.authName}}</el-tag>
+                                        <i class="el-icon-caret-right"></i>
+                                    </el-col>
+                                    <el-col :span="18">
+                                        <el-col>
+                                            <el-tag type="warning" :class="['bdbottom',i3 == 0?'':'bdtop']" v-for="(item3,i3) in item1.children" :key="item3.id">{{item3.authName}}</el-tag>
+                                        </el-col>                         
+                                    </el-col>
+                                </el-row>
+                            </el-col>
+                        </el-row>
+                        <!-- <pre>
+                            {{scope.row.children}}
+                        </pre> -->
+                    </template>
+                </el-table-column>
                 <!-- 索引列 -->
                 <el-table-column type='index'></el-table-column>
                 <el-table-column label="角色名称" prop="roleName"></el-table-column>
@@ -213,5 +242,13 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.el-tag {
+    margin: 7px;
+}
+.bdtop {
+    border-top: 1px solid #eee;
+}
+.bdbottom {
+    border-bottom: 1px solid #eee;
+}
 </style>
