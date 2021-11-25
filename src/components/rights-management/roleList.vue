6ebe3ec89beb727d4ab2e703fa697a9c32e1c90d
CXG <2226787396@qq.com>
2021-11-25 15:10:56 +0800

    【填充】-cxg

--- src/components/rights-management/roleList.vue
+++ src/components/rights-management/roleList.vue
@@ -12,7 +12,7 @@
         <el-card>
             <span>
                 <el-button type="primary" @click="addRoleVisible = true">添加角色</el-button>
-                <el-button type="primary">球球大伙别删权限，有bug，一删一片，人麻了已经</el-button>
+                <el-button type="primary">警告：球球大伙别删权限，有bug，一删一片，人麻了已经</el-button>
             </span>
 
             <el-table :data="roleList" border stripe>
@@ -51,7 +51,7 @@
                 <el-table-column type='index'></el-table-column>
                 <el-table-column label="角色名称" prop="roleName"></el-table-column>
                 <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
-                <el-table-column label="操作(编辑有bug点了无用）">
+                <el-table-column label="操作">
                     <template slot-scope="scope">
                         <!-- {{scope.row}} -->
                         <!-- 编辑按钮 -->
@@ -141,6 +141,7 @@ export default {
             editRole: false,
             //查询到的角色信息对象保存
             roleForm: {},
+            // roleId:'',
             //修改表单 的验证规则对象
             roleFormRules: {
                 roleName:[
@@ -175,7 +176,6 @@ export default {
                 return this.$message.error('获取角色列表失败');
             }
             this.roleList = res.data
-            // console.log(this.roleList);
         },
         //监听添加用户对话框的关闭事件
         roleListClosed() {
@@ -201,13 +201,13 @@ export default {
         },
         //展示编辑角色对话框
         async showRoleDialog(id) {
-            const { data: res} = await this.$http.get('roles/' + id)
+            const { data: res} = await this.$http.get('roles/' + id,)
             if(res.meta.status !==200) {
                 return this.$message.error('查询角色信息失败')
             }
+            // this.roleld =res.roleld
             this.roleForm = res.data;
             this.editRole = true;
-            // console.log(id);
         },
         //监听修改用户对话框的关闭事件
         roleRoleClosed() {
@@ -218,19 +218,20 @@ export default {
             this.$refs.roleFormRef.validate(async valid =>{
                 if(!valid) return
                 //发起修改用户信息的数据请求
-                const {data:res} = await this.$http.put('roles/' + this.roleForm.id,{
+                const {data:res} = await this.$http.put('roles/' + this.roleForm.roleId,{
+                    roleId:this.roleForm.roleId,
                     roleName:this.roleForm.roleName,
                     roleDesc:this.roleForm.roleDesc,
                 })
                 if(res.meta.status !== 200){
-                    return this.$message.error('更新角色信息失败')
+                    return this.$message.error('编辑角色信息失败')
                 }
                 //关闭对话框
                 this.editRole = false
                 //刷新数据列表
                 this.getRoleList()
                 //提示修改成功
-                this.$message.success('更新角色信息成功')
+                this.$message.success('编辑角色信息成功')
             }
             )
         },
@@ -245,7 +246,6 @@ export default {
             }).catch(err => err)//箭头函数里只有一行代码，所以简写
             //如果角色取消删除，则返回值为字符串 cancel
             //如果角色确认删除，则返回值为字符串 confirm
-            // console.log(confirmResult);
             if(confirmResult !== 'confirm') {
                 return this.$message.info('已取消删除')
             }
@@ -284,7 +284,6 @@ export default {
             }
             //获取到的权限数据保存到 data 中
             this.rightslist = res.data
-            console.log(this.rightslist);
             this.setRightDialogVisible = true;
             
         },
